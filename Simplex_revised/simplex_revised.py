9623a7bf80265aea1a9f43e40fa74b22522db8bd
Luis E. Cerna <cernalue@gmail.com>
2018-05-03 18:23:02 -0500

    ...

--- Simplex_revised/simplex_revised.py
+++ Simplex_revised/simplex_revised.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 from fractions import Fraction
 import argparse as parse
-
+import os
 # It's important not to stay in an infinit cycle that could
     # happen if there are repeated pivots. Code should be
     # able to identify and pick the next pivot, even if it
@@ -25,9 +25,13 @@ def writeTableu(tableu, n, mess):
     header = mess + ' ' + int(9*n)*'_' + '\n'
     footer = int(12*n) * '-' + '\n'
     #for row in tableu:
-    np.savetxt('output.txt', tableu, fmt='%5s',\
-    header=header, footer=footer)
-
+    dst = open('output.txt', 'a')
+    np.savetxt(dst, tableu, fmt='%5s', header=header, footer=footer)
+    
+    #np.savetxt(dst, tableu, fmt='%5s', header=header, footer=footer)
+    #dst = open('output.txt', 'ab')
+    #dst.writelines( [header, '\n', tableu, '\n', footer] ) #fmt='%5s'
+    dst.close() 
     return
 
 def opt_test(Z_vect):
@@ -37,14 +41,14 @@ def opt_test(Z_vect):
     
     return True
 
-def pivot(tableu, m, n, col, p_row):
+def pivot(tableu, m, n):
     table = tableu[0]
     table = table[:-1]
-    col = np.argmin(table)  
+    p_col = np.argmin(table)  
     i = True
     for row in range(1, int(m), 1):
         #print('\niteration ',row, '\n')
-        buffer = tableu[row][int(col)]
+        buffer = tableu[row][int(p_col)]
         #print('buff = ', buffer)
         b = tableu[row][int(n)-1]
         #print('b = ',b)
@@ -65,36 +69,50 @@ def pivot(tableu, m, n, col, p_row):
                 #print('test = ', test)
                 p_row = row
             i = False
-
-    return p
     
-def eta_vect(tableu, m, n, p_row, col, p):
-    eta_.ndarray( (4,1) )
-    for row in range(int(m))
+    return p, p_row, p_col
+    
+def eta_vect(tableu, m, n, p, p_row, p_col):
+    etav = np.ndarray()
+    etav = etav.astype('object')
+    for row in range(int(m)):
         if row == int(p_row):
-            a = int(tableu[row][int(col)])
-            eta_[int(row)] = p**(-1)
+            a = int(tableu[row][int(p_col)])
+            etav[row] = Fraction(1, int(p))
         else:
-            a = int(tableu[row][int(col)])
-            eta_[int(row)] = ( a ) / (- p)
+            a = int(tableu[row][int(p_col)])
+            #etav[int(row)] = ( a ) / (- p)
+            etav[row] = Fraction(a, int(-p) )
 
-    return
+    print (etav)
+    return etav
+
+def eta_tableu(etaV, p_col, m):
+    etaM = np.identity( int(m) )
+    for row in range(int(m)):
+        etaM[row][int(p_col)] = etaV[row]
     
-def Simplex(tableu, m, n):
+    return etaM
+
+def Simplex(tableu, m, n, etav):
+    #change etav declaration to this location
     t = opt_test(tableu[0])
     if t == True:
         return 0 # MUST CHANGE THIS FOR A FINALIZATION ROUTINE
     
-    p_row = 0 # If it stays a 0, then no pivot was found
-    col = 0
-    p_value = pivot(tableu, m, n, p_row, col)
-    eta_vect(tableu, m, n, p_row, col, p_value)
-    
-    
+    p = pivot(tableu, m, n)
+    etaV = eta_vect(tableu, m, n, p[0], p[1], p[2]) #gives eta as a row
+
+    etaM = eta_tableu(etaV, p[2], m)
+    #print(etaM)
+    T = np.matmul(etaM, tableu)
+    writeTableu(etaM, n, 'eta Tableu')
+    writeTableu(T, n, 'Iteration 1')
     
     return
 
 ################################## MAIN PROGRAM
+os.system('> output.txt')
 table = np.ndarray( (20,20) )
 
 table = np.loadtxt("file.txt", dtype=float, comments='#', delimiter=" ")
@@ -102,12 +120,13 @@ m = table[0][0]
 n = table[1][0]
 table = table[2:]
 tableu = incrementedTableu(table, m, n)
+etav = np.ndarray( 4 )
 
 writeTableu(tableu, n, "\nIteration 0")
 
 n = (n-1) + m #Now n has the incremented tableu size, not the original
 
-Simplex(tableu, m, n)
+Simplex(tableu, m, n, etav)
 
 #size = np.fromfile("file.txt", dtype = float, count = 2, sep = " ")
 #tableu = np.fromfile("file.txt", dtype = float, count = -1, sep = " ")
