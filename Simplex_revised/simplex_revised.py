3e914b274fb9abe80e7f8f1e9337c56a736e438a
Luis E. Cerna <cernalue@gmail.com>
2018-05-04 22:23:01 -0500

    Made the methodsthat I used to call from numpy, because they're crap. Next is to check the multiplication of matrices I made because the output isn't right

--- Simplex_revised/simplex_revised.py
+++ Simplex_revised/simplex_revised.py
@@ -1,27 +1,68 @@
 import sys
 import numpy as np
-from fractions import Fraction
 import argparse as parse
 import os
-# It's important not to stay in an infinit cycle that could
+from sympy import *
+from decimal import Decimal
+
+# CHECK ITERATION 1, IT'S WRONG
+
+    # It's important not to stay in an infinit cycle that could
     # happen if there are repeated pivots. Code should be
     # able to identify and pick the next pivot, even if it
     # has the same value
 
-# Also, it is of importance 
+def getconst(table, i, j):
+    m = int(i)
+    n = int(j)
+    b = np.ndarray( [m,1] )
+    for row in range(m):
+        b[row][0] = table[row][n-1]
 
-def incrementedTableu(table, m, n):
-    b = np.hsplit(table, int(n))
-    b = b[int(n)-1]
-    table = np.hsplit(table, np.array( [int(m)-1,int(n)]) )
-    table = table[0]
-    identity = np.vstack( (np.zeros( (1, int(n)-1) ), np.identity( int(m)-1 )) )
-    identity_b = np.hstack ( [identity, b] )
-    tableu = np.hstack( (table, identity_b) ) #tableu[:] = np.hstack( [table, np.identity(int(n) ])
+    return b
+
+def hsplit(table, i, j):
+    m = int(i)
+    n = int(j)
+    tableu = np.ndarray( [m,n-1] )
+    for row in range(m):
+        for col in range(n-1):
+            tableu[row][col] = table[row][col]
 
     return tableu
+
+def hstack(A, B):
+    a, b = A.shape, B.shape
+    m, n = a[0], a[1]+b[1]
+    C = np.ndarray( [m,n] )
+    for i in range(m):
+        l, k = -1, -1
+        for j in range(n):
+            if j < a[1]:
+                l += 1
+                C[i][j] = A[i][l]
+            elif j >= a[1]:
+                k += 1
+                C[i][j] = B[i][k]
+
+    return C
+
+def incrementedTableu(table, m, n):
+    b = getconst(table, m, n)
+    table = hsplit(table, m, n)
+    identity = np.vstack( (np.zeros( (1, int(m)-1) ), np.identity( int(m)-1 )) )
+    identity_b = np.hstack( [identity, b] )
+    tableu = hstack(table, identity_b)
     
-def writeTableu(tableu, n, mess):
+    return tableu
+    
+def writeTableu(tableu, m, n, mess):
+    size = tableu.shape
+    tableu = tableu.astype('object')
+    for i in range(int(size[0])):
+        for j in range(int(size[1])):
+            tableu[i][j] = tableu[i][j]
+    
     header = mess + ' ' + int(9*n)*'_' + '\n'
     footer = int(12*n) * '-' + '\n'
     #for row in tableu:
@@ -46,6 +87,7 @@ def pivot(tableu, m, n):
     table = table[:-1]
     p_col = np.argmin(table)  
     i = True
+    print (tableu)
     for row in range(1, int(m), 1):
         #print('\niteration ',row, '\n')
         buffer = tableu[row][int(p_col)]
@@ -73,26 +115,41 @@ def pivot(tableu, m, n):
     return p, p_row, p_col
     
 def eta_vect(tableu, m, n, p, p_row, p_col):
-    etav = np.ndarray()
+    etav = np.ndarray(int(m))
     etav = etav.astype('object')
     for row in range(int(m)):
         if row == int(p_row):
             a = int(tableu[row][int(p_col)])
-            etav[row] = Fraction(1, int(p))
+            etav[row] = Rational(1, int(p))
         else:
             a = int(tableu[row][int(p_col)])
             #etav[int(row)] = ( a ) / (- p)
-            etav[row] = Fraction(a, int(-p) )
+            etav[row] = Rational(a, int(-p) )
 
-    print (etav)
     return etav
 
-def eta_tableu(etaV, p_col, m):
+def eta_tableu(etaV, p_row, m):
+    print('m =', m)
     etaM = np.identity( int(m) )
+    etaM = etaM.astype('object')
     for row in range(int(m)):
-        etaM[row][int(p_col)] = etaV[row]
-    
+        etaM[row][int(p_row)] = Rational(etaV[row])
+
     return etaM
+
+def matmul(A, B):
+    a, b = A.shape, B.shape
+    m, r, n = a[0], a[1], b[1]
+    c = np.ndarray( [m,n] )
+    
+    for i in range(m):
+        for j in range(n):
+            for k in range(r):
+                c[i][j] += A[i][k] * B[k][j]
+                c[i][j] = round(c[i][j], 2)
+                c[i][j] = Rational(c[i][j])
+    
+    return c
 
 def Simplex(tableu, m, n, etav):
     #change etav declaration to this location
@@ -103,17 +160,17 @@ def Simplex(tableu, m, n, etav):
     p = pivot(tableu, m, n)
     etaV = eta_vect(tableu, m, n, p[0], p[1], p[2]) #gives eta as a row
 
-    etaM = eta_tableu(etaV, p[2], m)
-    #print(etaM)
-    T = np.matmul(etaM, tableu)
-    writeTableu(etaM, n, 'eta Tableu')
-    writeTableu(T, n, 'Iteration 1')
+    etaM = eta_tableu(etaV, p[1], m)
+    writeTableu(etaM, m, n, 'eta Tableu')
+    etaM = etaM.astype('float')
+    T = matmul(etaM, tableu)
+    writeTableu(T, m, n, 'Iteration 1')
     
     return
 
 ################################## MAIN PROGRAM
 os.system('> output.txt')
-table = np.ndarray( (20,20) )
+table = np.ndarray( (20,20) ) 
 
 table = np.loadtxt("file.txt", dtype=float, comments='#', delimiter=" ")
 m = table[0][0]
@@ -122,10 +179,8 @@ table = table[2:]
 tableu = incrementedTableu(table, m, n)
 etav = np.ndarray( 4 )
 
-writeTableu(tableu, n, "\nIteration 0")
-
 n = (n-1) + m #Now n has the incremented tableu size, not the original
-
+writeTableu(tableu, m, n, "\nIteration 0")
 Simplex(tableu, m, n, etav)
 
 #size = np.fromfile("file.txt", dtype = float, count = 2, sep = " ")
